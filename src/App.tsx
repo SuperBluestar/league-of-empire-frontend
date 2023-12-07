1f6495c1479ba93df6c22d014a65230e8d95a8d6
Superbluestar <alexander116gm@gmail.com>
2023-12-07 18:48:00 +0900

    Init commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes } from "react-router-dom";
+import { HomeImg } from './pages/HomeImg';
+import { Home } from './pages/Home';
+import { ItemDetail } from './pages/ItemDetail';
+import { MarketPlace } from './pages/MarketPlace';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/itemdetail" element={<ItemDetail />} />
+      <Route path="/marketplace" element={<MarketPlace />} />
+      <Route path="/home-img" element={<HomeImg />} />
+    </Routes>
   );
 }
 
